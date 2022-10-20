b38f89e58297853748e333467a54f1c0447361a9
Desta Fajri Dwi Purnomo <destafajri@gmail.com>
2022-10-20 13:35:09 +0700

    push BE work

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
-import(
-	"fmt"
+import (
+	// "fmt"
+	"log"
+	"net/http"
 
+	"github.com/destafajri/video-chat-app/server"
 )
 func main() {
-	fmt.Println("hello")	
+	server.AllRooms.Init()
+
+	http.HandleFunc("/create", server.CreateRoomHandler)
+	http.HandleFunc("/join", server.JoinRoomHandler)
+
+	err := http.ListenAndServe(":1000", nil)
+	if err !=nil {
+		log.Fatal(err)
+	}
+
 }
